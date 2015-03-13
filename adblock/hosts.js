09c2609e49ffee212e93662a1d555d5889c07779
James Edward Lewis II <lewisje@users.noreply.github.com>
2015-03-13 09:54:35 -0400

    Refer to GitHub-hosted file

--- adblock/hosts.js
+++ adblock/hosts.js
@@ -1,17 +1,15 @@
-http=new ActiveXObject('Microsoft.XMLHTTP');
-fso=new ActiveXObject('Scripting.FileSystemObject');
-http.open('GET',website='https://jansal.googlecode.com/svn/trunk/adblock/hosts',false);
-http.setRequestHeader('Accept','text/html');
+var http = new ActiveXObject('Microsoft.XMLHTTP'), fso = new ActiveXObject('Scripting.FileSystemObject'),
+  v = new ActiveXObject('Shell.Application'), f = fso.CreateTextFile(file = 'C:\\WINDOWS\\System32\\DRIVERS\\etc\\hosts', true);
+http.open('GET', website = 'https://raw.githubusercontent.com/lewisje/jansal/master/adblock/hosts', false);
+http.setRequestHeader('Accept', 'text/plain');
 http.send();
-f=fso.CreateTextFile(file='C:\\WINDOWS\\System32\\DRIVERS\\etc\\hosts',true);
 f.Write(http.responseText.replace(/0.0.0.0 /g,'127.0.0.2 '));
 f.Close();
-//http.open('GET',website='http://pgl.yoyo.org/as/serverlist.php?hostformat=proxyautoconfig&useip=127.0.0.2&mimetype=plaintext',false);
-//http.setRequestHeader('Accept','text/html');
+//http.open('GET', website = 'http://pgl.yoyo.org/as/serverlist.php?hostformat=proxyautoconfig&useip=127.0.0.2&mimetype=plaintext', false);
+//http.setRequestHeader('Accept', 'text/plain');
 //http.send();
-//f=fso.CreateTextFile(file='C:\\WINDOWS\\System32\\DRIVERS\\etc\\proxy',true);
+//f = fso.CreateTextFile(file = 'C:\\WINDOWS\\System32\\DRIVERS\\etc\\proxy', true);
 //f.Write(http.responseText);
 //f.Close();
-v=new ActiveXObject('Shell.Application');
-v.ShellExecute('nbtstat','-R','','open',0);
-v.ShellExecute('ipconfig','/displaydns','','open',0);
+v.ShellExecute('nbtstat', '-R', '', 'open', 0);
+v.ShellExecute('ipconfig', '/displaydns', '', 'open', 0);
