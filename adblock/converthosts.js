6b51c83dc73d6825ffd382c5be4cbc4ce050c246
James Edward Lewis II <lewisje@users.noreply.github.com>
2016-09-05 18:03:34 -0400

    prepare for potential IPv6-supporting hosts files

--- adblock/converthosts.js
+++ adblock/converthosts.js
@@ -1,7 +1,7 @@
 var fso = new ActiveXObject('Scripting.FileSystemObject');
-function convert(inf, out, bh, lim){
+function convert(inf, out, bh, b6, lim){
   'use strict';
-  var s, f, i;
+  var l = [], s, f, i, t;
   if (!fso.FileExists(inf)) return;
   s=fso.OpenTextFile(file=inf, 1);
   f=fso.CreateTextFile(file=out, true);
@@ -14,17 +14,17 @@ function convert(inf, out, bh, lim){
   f.WriteLine('255.255.255.255 broadcasthost');
   f.WriteLine('46.246.119.139 status.block.aid');
   while (!s.AtEndOfStream) {
-    f.Write(bh);
-    for (i = lim; i > 0 && !s.AtEndOfStream; i--) {
-      f.Write(' ' + s.readLine().replace('0.0.0.0 ' , ''));
-    }
-  f.Write('\n');
+    l.length = 0;
+    for (i = lim; i-- && !s.AtEndOfStream;) l.push(s.readLine().replace('0.0.0.0 ' , ''));
+    t = l.join(' ');
+    f.WriteLine(bh + ' ' + t);
+    //f.WriteLine(b6 + ' ' + t);
   }
   s.Close();
   f.Close();
 }
-convert('hosts.txt', 'hostslt.txt', '0.0.0.0', 24);
-convert('hostsu', 'hostsc', '0.0.0.0', 24);
-convert('hostsbkp', 'hosts', '0.0.0.0', 9);
-convert('hostsbk', 'hostslt', '192.168.1.254', 9);
-convert('hostsbig', 'hostslrg', '0.0.0.0', 24);
+convert('hosts.txt', 'hostslt.txt', '0.0.0.0', '::', 24);
+convert('hostsu', 'hostsc', '0.0.0.0', '::', 24);
+convert('hostsbkp', 'hosts', '0.0.0.0', '::', 9);
+convert('hostsbk', 'hostslt', '192.168.1.254', '::', 9);
+convert('hostsbig', 'hostslrg', '0.0.0.0', '::', 24);
